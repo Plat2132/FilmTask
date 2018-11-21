e990d034ab9db4757e0de86a61b6507963edded0
Plat2132 <44614865+Plat2132@users.noreply.github.com>
2018-11-22 00:35:58 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         Film film = new Film("Cube");
+
         System.out.println(film.name);
+        System.out.println("Genre thrille");
         film.setDirector("\tVincenzo Natali");
         film.setDuration(" 90 min");
 
         Film filmTwo = new Film("Iron man");
         System.out.println(filmTwo.name);
+        System.out.println("Genre adventure");
         film.setDirector("\tJon Favreau");
         film.setDuration(" 126 min");
 
         Film filmThird = new Film("Scarface");
         System.out.println(filmThird.name);
+        System.out.println("Genre crime");
         film.setDirector("\tBrian De Palma");
         film.setDuration(" 170 min");
 
